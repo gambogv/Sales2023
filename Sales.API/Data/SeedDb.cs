6096677180d340bbcdaae1cf155753700562c0b5
GABRIEL ANGEL VELASQUEZ RINCON <gabrielvelasquez256153@correo.itm.edu.co>
2023-05-28 11:06:57 -0500

    Seed Db V1

---
+++ Sales.API/Data/SeedDb.cs
@@ -0,0 +1,33 @@
+﻿using Sales.Shared.Entites;
+
+
+namespace Sales.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckCountriesAsync();
+        }
+
+        private async Task CheckCountriesAsync()
+        {
+            if (!_context.Countries.Any())
+            {
+                _context.Countries.Add(new Country { Name = "Colombia" });
+                _context.Countries.Add(new Country { Name = "Estados Unidos" });
+                _context.Countries.Add(new Country { Name = "España" });
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
